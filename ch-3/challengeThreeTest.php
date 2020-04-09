b1c4c775d6d0c8a9ee35a66c847dc8947f08a999
shaphan <chaphannzabonimana@gmail.com>
2020-04-09 16:48:33 +0200

    Fix assertion error for challenge 2 & 3

--- ch-3/challengeThreeTest.php
+++ ch-3/challengeThreeTest.php
@@ -46,9 +46,10 @@ class challengeThreeTest extends TestCase
 
     $values = valueOnFields($result, $estimate, $challenge);
     foreach ($values as $estimation) {
-      $produced = $estimation[0];
-      $expected = $estimation[1];
-      $this->assertEquals($produced, $expected);
+      $field = $estimation[0];
+      $produced = $estimation[1];
+      $expected = $estimation[2];
+      $this->assertEquals($produced, $expected, 'The expected ' . $field . ' ' . $expected . ' is not equal to your output ' . $produced);
     }
   }
 }
